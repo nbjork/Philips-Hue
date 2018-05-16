0e41068bb41e715bf76c223566365c260a23bfab
Nishadh Aluthge <nishadh.aluthge@helsinki.fi>
2018-05-16 14:12:54 +0100

    changed buld ID naming

--- Hue.py
+++ Hue.py
@@ -3,8 +3,8 @@
 from phue import Bridge
 from time import sleep
 
-IP = "192.168.1.168"
-b = Bridge(IP)
+IP = "192.168.1.168"        # Ip address of the Philips Hue bridge
+b = Bridge(IP)              # Instance of Bridge class
 
 
 def connect(b):
@@ -18,16 +18,19 @@ def list_bulbs(b):
     lights = b.lights
     for l in lights:        # Print list of light names
         print(l.name)
+    return lights
 
 
-def set_params(b, light_ID, param, value):
+def set_params(light_ID, param, value):
     # Example b.set_light(2,'on', True), b.set_light(2, 'bri', 200)
     b.set_light(light_ID, param, value)
 
 
 # Operating the Hue bulb
 connect(b)      # connecting to the Huw bulb
-list_bulbs(b)   # lisitng available bulbs
-set_params(2,'on', True)
+bulbs = list_bulbs(b)   # listing available bulbs
+set_params(bulbs[0], 'on', True)        # changing bulb parameters  - Switching ON
 sleep(4)
-set_params(2, 'bri', 200)
+set_params(bulbs[0], 'bri', 200)        # changing bulb parameters - Adjust brightness
+sleep(4)
+set_params(bulbs[0], 'on', False)       # changing bulb parameters - Switching OFF
